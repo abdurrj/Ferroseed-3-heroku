c99381d2053c63077428dd4f3f777e952c8abc92
abdurrj <68663788+abdurrj@users.noreply.github.com>
2021-12-28 20:13:22 +0100

    m

--- main/modules/UserGuildJoin.py
+++ main/modules/UserGuildJoin.py
@@ -4,7 +4,8 @@ class UserGuildJoin(commands.Cog):
     def __init__(self, client):
         self.client = client
 
-    @commands.command()
+
+    @commands.command(hidden=True)
     @commands.has_permissions(administrator=True)
     async def setWelcomeChannel(self, ctx, welcomeChannel:nextcord.TextChannel=None):
         data = getJson(guildSettingsPath)
@@ -19,7 +20,7 @@ class UserGuildJoin(commands.Cog):
             await ctx.send("Welcome message not yet set. Remember to set welcome message")
         
 
-    @commands.command()
+    @commands.command(hidden=True)
     @commands.has_permissions(administrator=True)
     async def setWelcomeMessage(self, ctx, welcomeMessage:nextcord.Message=None):
         data = getJson(guildSettingsPath)
@@ -31,7 +32,7 @@ class UserGuildJoin(commands.Cog):
         await ctx.send(f"Welcome message set to: {welcomeMessage.jump_url if welcomeMessage else None}")
         
 
-    @commands.command()
+    @commands.command(hidden=True)
     @commands.has_permissions(administrator=True)
     async def sendWelcomeMessage(self, ctx):
         data = getJson(guildSettingsPath)
@@ -44,29 +45,53 @@ class UserGuildJoin(commands.Cog):
         welcomeMessage = await welcomeMessageFromChannel.fetch_message(welcomeMessageId)
         await ctx.send(welcomeMessage.content)
 
-    @commands.command()
+
+    @commands.command(hidden=True)
     @commands.has_permissions(administrator=True)
     async def sendWelcomeMessageOnMemberJoin(self, ctx, sendMessage:str="False"):
         data = getJson(guildSettingsPath)
         guildSettings = data[str(ctx.guild.id)]
         if sendMessage.lower().capitalize=="False":
             guildSettings["sendWelcomeMessage"] = "False"
-            await ctx.send("I will now send:")
+            await ctx.send("I will not send a message announcing new members")
         elif sendMessage.lower().capitalize=="True":
             guildSettings["sendWelcomeMessage"] = "True"
+            await ctx.send("I will now send a message announcing new members if you have set a channel and message")
         data[str(ctx.guild.id)] = guildSettings
         writeJson(guildSettingsPath, data)
-        
 
 
-    @commands.command()
+    @commands.command(hidden=True)
     @commands.has_permissions(administrator=True)
     async def setWelcomeDmMessage(self, ctx, welcomeDmMessage:nextcord.Message=None):
-        pass
+        data = getJson(guildSettingsPath)
+        guildSettings = data[str(ctx.guild.id)]
+        guildSettings["welcomeDmMessage"] = welcomeDmMessage.id if welcomeDmMessage else None
+        guildSettings["welcomeDmMessageFromChannel"] = welcomeDmMessage.channel.id if welcomeDmMessage else None
+        data[str(ctx.guild.id)] = guildSettings 
+        writeJson(guildSettingsPath, data)
+        await ctx.send(f"Welcome message set to: {welcomeDmMessage.jump_url if welcomeDmMessage else None}")
 
-    @commands.command()
+
+    @commands.command(hidden=True)
     @commands.has_permissions(administrator=True)
-    async def sendWelcomeDmOnMemberJoin(self, ctx, sendMessage:bool=False):
+    async def sendWelcomeDmOnMemberJoin(self, ctx, sendMessage:str="False"):
+        data = getJson(guildSettingsPath)
+        guildSettings = data[str(ctx.guild.id)]
+        if sendMessage.lower().capitalize=="False":
+            guildSettings["sendWelcomeDm"] = "False"
+            await ctx.send("I will not send a DM to new members")
+        elif sendMessage.lower().capitalize=="True":
+            guildSettings["sendWelcomeDm"] = "True"
+            await ctx.send("I will now send a DM to new members if you have set a DM message.")
+        data[str(ctx.guild.id)] = guildSettings
+        writeJson(guildSettingsPath, data)
+
+
+    ## Event listener
+
+    @commands.Cog.listener("on_member_join")
+    async def welcomeNewMemberEvent(self, member):
         pass
 
 def setup(client):
